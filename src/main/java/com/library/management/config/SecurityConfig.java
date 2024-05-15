d1c80a4314250b12333e7aded57cf54fbb23789b
yashgirhe <girheyash@gmail.com>
2024-05-15 17:33:39 +0530

    implemented basic auth

--- src/main/java/com/library/management/config/SecurityConfig.java
+++ src/main/java/com/library/management/config/SecurityConfig.java
@@ -6,7 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +20,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
+                        (authz) -> authz
+                                .requestMatchers(new AntPathRequestMatcher("/public/**")).hasRole("USER")
+                                .anyRequest()
+                                .authenticated())
                 .httpBasic(Customizer.withDefaults());//security type - basic
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user1 = User.withUsername("yash").username("yash").password(passwordEncoder().encode("yash")).roles("ADMIN").build();
+        UserDetails user2 = User.withUsername("ronaldo").username("ronaldo").password(passwordEncoder().encode("ronaldo")).roles("USER").build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
